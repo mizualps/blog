ede7fc962645caf9ff364e508613847d69a0ad42
mizuguchi <mizuguchi@alpstool.com>
2016-11-30 14:48:29 +0900

    add easyAdmin + auth

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,7 +20,8 @@ class AppKernel extends Kernel
             new Acme\StoreBundle\AcmeStoreBundle(),
             new FOS\RestBundle\FOSRestBundle(),
             new JMS\SerializerBundle\JMSSerializerBundle(),
-            new FOS\UserBundle\FOSUserBundle()
+            new FOS\UserBundle\FOSUserBundle(),
+            new JavierEguiluz\Bundle\EasyAdminBundle\EasyAdminBundle()
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'), true)) {
